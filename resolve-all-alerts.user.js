01d1dba782ef82cabd037e7c5ae3028380c3f208
Jesse Patching <taeram@users.noreply.github.com>
2013-12-10 09:17:46 -0700

    Update resolve-all-alerts.user.js

--- resolve-all-alerts.user.js
+++ resolve-all-alerts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Resolve All Alerts
 // @namespace     https://github.com/taeram/user-scripts
-// @version       1.0.0
+// @version       1.0.2
 // @description   Resolve all alerts on the Alert Management page
 // @match         https://*.blacklinegps.com/alert
 // @license       MIT https://github.com/taeram/user-scripts/blob/master/LICENSE
@@ -10,35 +10,30 @@
 // ==/UserScript==
 
 // Add the button
-var button = $('<button class="btn btn-info" style="margin-left: 15px">Resolve All (JavaScript)</button>').appendTo('h1');
+var button = $('<button class="btn btn-info" style="margin-left: 15px">Resolve All</button>').appendTo('h1');
 
 var numAlerts = 0;
 var alertCounter = 0;
 $(button).click(function () {
+    $(this).attr('disabled', true);
     numAlerts = $('.alertStatus  option[value="unacknowledged"]').length;
     resolveNextAlert();
 });
 
 function resolveNextAlert() {
-    if (alertCounter > 5) {
-        console.log("Stopping");
-        return;
-    }
-
     // Get the next unacknowledged alert
     var select = $('.alertStatus option[value="unacknowledged"]').parent().first();
-    if (select) {
+    if (select.length > 0) {
         alertCounter++;
-
         console.log("Resolving alert " + alertCounter + " of " + numAlerts);
 
-        // Remove all 'selected' attributes
+        // Remove all 'selected' attributes from the options
         $(select).parent().find('option').removeAttr('selected');
 
-        // Acknowledge it
+        // Select the "resolved" option
         $(select).find('option[value="resolved"]').attr('selected', 'selected');
 
-        // Tell chosen we've changed the select box
+        // Trigger the change event, which triggers the AJAX request
         $(select).change();
 
         // Wait for the request to complete
@@ -49,12 +44,14 @@ function resolveNextAlert() {
 }
 
 function waitForRequestToComplete(select, callback) {
+    // Is the ajax spinner icon still visible?
     if ($(select).parent().find('.icon-spinner:visible').length > 0) {
-        console.log("Waiting for request to complete");
         setTimeout(waitForRequestToComplete, 100, select, callback);
     } else {
-        // Remove the select box
+        // Remove the select box from the page so we don't attempt to resolve it twoice
         $(select).remove();
+        
+        // Callback!
         callback();
     }
 }
