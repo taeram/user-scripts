a32cd7ecb9e41eab1fd359bb818cff8dbe9a321c
Jesse Patching <taeram@users.noreply.github.com>
2013-12-12 10:41:19 -0700

    Update and rename tidy-bookos.user.js to tidy-bookza.user.js

--- tidy-bookza.user.js
+++ tidy-bookza.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name       Tidy BookOS.org
+// @name       Tidy bookza.org
 // @namespace  http://github.com/taeram/
 // @version    1.0.0
-// @description  Tidy BookOS.org search results
-// @match      http://bookos.org/s/*
+// @description  Tidy bookza.org search results
+// @match      http://bookza.org/s/*
 // @copyright  2013 Jesse Patching
 // @license    MIT http://opensource.org/licenses/MIT
-// @updateURL   https://raw.github.com/taeram/user-scripts/master/tidy-bookos.user.js
-// @downloadURL https://raw.github.com/taeram/user-scripts/master/tidy-bookos.user.js
+// @updateURL   https://raw.github.com/taeram/user-scripts/master/tidy-bookza.user.js
+// @downloadURL https://raw.github.com/taeram/user-scripts/master/tidy-bookza.user.js
 // ==/UserScript==
 
 // Remove invalid search results
