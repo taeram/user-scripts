0a8be3be1374b4e169437a3e879a191c04872b28
Jesse Patching <taeram@users.noreply.github.com>
2014-05-20 11:12:55 -0600

    Update tidy-theonion.user.js

--- tidy-theonion.user.js
+++ tidy-theonion.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          The Onion
 // @namespace     https://github.com/taeram/user-scripts
-// @version       1.0.1
+// @version       1.0.2
 // @description   Tidies theonion.com
 // @match         http://*.theonion.com/*
 // @license       MIT https://github.com/taeram/user-scripts/blob/master/LICENSE
@@ -9,28 +9,12 @@
 // @downloadURL   https://raw.github.com/taeram/user-scripts/master/tidy-theonion.user.js
 // ==/UserScript==
 
-function tidy() {
-    // Stop if we haven't found the gregbox after a number of retries
-    timerCount++;
-    if (timerCount > 50) {
-        return clearTimeout(timer);
-    }
-
-    // Do we have a gregbox?
-    var gregbox = $('#gregbox-outer');
-    if ($(gregbox).length > 0) {
-        $(gregbox).next().remove()
-        $(gregbox).next().remove()
-        $(gregbox).next().remove()
-        $(gregbox).remove();
-
-        return clearTimeout(timer);
-    }
-
-    // Nothing found yet, keep trying
-    setTimeout(tidy, 500);
-}
-
-// Cevin Key Attack Mode Go!
-var timerCount = 0;
-tidy();
+// Hide the overlays
+$('head').append(
+    '<style type="text/css">' + 
+        '#pressplusOverlay, #ppUI {' + 
+            'display: none;' + 
+        '}' +
+    '</style>'
+);
+    
