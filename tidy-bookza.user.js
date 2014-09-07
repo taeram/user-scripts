66124e4d9ce166ad504134c9930e49844a675c43
Jesse Patching <taeram@users.noreply.github.com>
2014-09-06 21:17:18 -0600

    Update tidy-bookza.user.js

--- tidy-bookza.user.js
+++ tidy-bookza.user.js
@@ -2,8 +2,8 @@
 // @name       Tidy bookza.org
 // @namespace  http://github.com/taeram/
 // @version    1.0.0
-// @description  Tidy bookza.org search results
-// @match      http://bookza.org/s/*
+// @description  Tidy bookzz.org search results
+// @match      http://bookzz.org/s/*
 // @copyright  2013 Jesse Patching
 // @license    MIT http://opensource.org/licenses/MIT
 // @updateURL   https://raw.github.com/taeram/user-scripts/master/tidy-bookza.user.js
