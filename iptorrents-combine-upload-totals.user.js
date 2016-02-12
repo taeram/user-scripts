82df1b4ed10300ae162a169f121728490ddc8edc
Jesse Patching <taeram@users.noreply.github.com>
2016-02-12 11:41:26 -0700

    Create iptorrents-combine-upload-totals.user.js

---
+++ iptorrents-combine-upload-totals.user.js
@@ -0,0 +1,91 @@
+// ==UserScript==
+// @name            IPTorrents - Combine Upload Totals
+// @namespace       http://github.com/taeram/user-scripts
+// @description     Combine "Uploaded" totals on the Peers page
+// @match           https://www.iptorrents.com/peers?*;o=4
+// @grant           none
+// @copyright       Jesse Patching
+// @version         1.0.0
+// @license         MIT https://github.com/taeram/user-scripts/blob/master/LICENSE
+// @updateURL       https://raw.github.com/taeram/user-scripts/master/iptorrents-combine-upload-totals.user.js
+// @downloadURL     https://raw.github.com/taeram/user-scripts/master/iptorrents-combine-upload-totals.user.js
+// ==/UserScript==
+
+/* jshint -W097 */
+'use strict';
+
+var sortedRows = [];
+var rows = $('table.t1 tr');
+for (var i=2; i < rows.length; i++) {
+    var uploadedEl = $(rows[i]).find('td:nth-child(4)');
+    var uploaded = uploadedEl.text().match(/([\d+\.]+) (\w+) \(([\d+\.]+) (\w+)\)$/);
+    if (uploaded) {
+        var currentUploaded = parseFloat(uploaded[1]);
+        if (uploaded[2] == 'GB') {
+            currentUploaded *= 1000;
+        }
+        
+        var oldUploaded = parseFloat(uploaded[3]);
+        if (uploaded[4] == 'GB') {
+            oldUploaded *= 1000;
+        }
+        
+        var totalUploaded = parseFloat(currentUploaded + oldUploaded);
+        while (sortedRows[totalUploaded]) {
+            sortedRows[totalUploaded]++;
+        }
+        sortedRows[Math.round(totalUploaded)] = rows[i];
+        
+        var label = 'MB';
+        if (totalUploaded > 1000) {
+            label = 'GB';
+            totalUploaded /= 1000;
+            totalUploaded = parseFloat(totalUploaded, 2).toFixed(2);
+        }
+        $(uploadedEl).html(totalUploaded + ' ' + label);
+    }
+}
+sortedRows = ksort(sortedRows);
+
+for (var i in sortedRows) {
+    var html = $(sortedRows[i])[0].outerHTML;
+    sortedRows[i].remove();
+    $('table.t1 tr:nth-child(1)').after(html);
+}
+
+function ksort(inputArr) {    
+  var tmp_arr = {},
+    keys = [],
+    sorter, i, k, that = this,
+    strictForIn = false,
+    populateArr = {};
+
+    // compare items numerically
+    sorter = function(a, b) {
+      return ((a + 0) - (b + 0));
+    };
+
+  // Make a list of key names
+  for (k in inputArr) {
+    if (inputArr.hasOwnProperty(k)) {
+      keys.push(k);
+    }
+  }
+  keys.sort(sorter);
+
+  // Rebuild array with sorted key names
+  for (i = 0; i < keys.length; i++) {
+    k = keys[i];
+    tmp_arr[k] = inputArr[k];
+    if (strictForIn) {
+      delete inputArr[k];
+    }
+  }
+  for (i in tmp_arr) {
+    if (tmp_arr.hasOwnProperty(i)) {
+      populateArr[i] = tmp_arr[i];
+    }
+  }
+
+  return populateArr;
+}
