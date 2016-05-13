fded6821057b1c32aa0e4d0a4dc868c36602adc3
Jesse Patching <taeram@users.noreply.github.com>
2016-05-12 21:29:51 -0600

    Added event and weapon handling

--- a-dark-room.user.js
+++ a-dark-room.user.js
@@ -5,7 +5,7 @@
 // @match           http://adarkroom.doublespeakgames.com/
 // @grant           none
 // @copyright       Jesse Patching
-// @version         2.0.0
+// @version         2.1.0
 // @license         MIT https://github.com/taeram/user-scripts/blob/master/LICENSE
 // @updateURL       https://raw.github.com/taeram/user-scripts/master/a-dark-room.user.js
 // @downloadURL     https://raw.github.com/taeram/user-scripts/master/a-dark-room.user.js
@@ -37,6 +37,69 @@
 
                 $(things[i]).click();
             }
+        },
+
+        handleEvent: function () {
+            if ($('#event').length > 0) {
+                // Allow the old wanderer to stay the night
+                if ($('#agree').length > 0) {
+                    $('#agree').click();
+
+                    // Learn scouting
+                    $('#learn').click();
+
+                    // Buy a shit load of maps
+                    if ($('#buyMap').length > 0) {
+                        var numMapsBought = 0;
+                        while (!$('#buyMap').hasClass('disabled')) {
+                            $('#buyMap').click();
+                            numMapsBought++;
+                        }
+                        console.log("Bought " + numMapsBought + " maps");
+                    }
+                }
+
+                // Spare the thief
+                if ($('#spare').length > 0) {
+                    $('#spare').click();
+                    $('#leave').click();
+                }
+
+                // Mourn the dead
+                if ($('#mourn').length > 0) {
+                    $('#mourn').click();
+                }
+
+                // Track the prints into the forest
+                if ($('#track').length > 0) {
+                    $('#track').click();
+                    $('#end').click();
+                }
+
+                // Go home after the beasts attack
+                if ($('#end').length > 0) {
+                    $('#end').click();
+                }
+
+                // Investigate the scratching noises
+                if ($('#investigate').length > 0) {
+                    $('#investigate').click();
+                    $('#leave').click();
+                }
+
+                // Give the beggar some furs
+                if ($('#100furs').length > 0) {
+                    $('#100furs').click();
+                    $('#50furs').click();
+                    $('#leave').click();
+                }
+            }
+        },
+
+        handleFighting: function () {
+            if ($('#event').length > 0) {
+                $('.weaponButton').click();
+            }
         }
     };
 
@@ -51,4 +114,10 @@
     // Build all the things
     ADR.intervals.buildThings = setInterval(ADR.buildThings, 30000);
     ADR.buildThings();
+
+    // Handle any incoming events
+    ADR.intervals.handleEvent = setInterval(ADR.handleEvent, 1000);
+
+    // Handle any fighting
+    ADR.intervals.handleFighting = setInterval(ADR.handleFighting, 250);
 })();
