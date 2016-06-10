d5f3a14a0695a0b8908cce8ce9e7ec4d9da8995d
Jesse Patching <taeram@users.noreply.github.com>
2016-06-10 15:15:01 -0600

    Use anonymous function wrapper to load after source page

--- iptorrents-time-until-spend-bonus-points.user.js
+++ iptorrents-time-until-spend-bonus-points.user.js
@@ -6,22 +6,23 @@
 // @match           https://iptorrents.com/*
 // @grant           none
 // @copyright       Jesse Patching
-// @version         1.2.0
+// @version         1.2.1
 // @license         MIT https://github.com/taeram/user-scripts/blob/master/LICENSE
 // @updateURL       https://raw.github.com/taeram/user-scripts/master/iptorrents-time-until-spend-bonus-points.user.js
 // @downloadURL     https://raw.github.com/taeram/user-scripts/master/iptorrents-time-until-spend-bonus-points.user.js
 // ==/UserScript==
 
-/* jshint -W097 */
-'use strict';
+(function() {
+    'use strict';
 
-var secondsPerBonusPoint = (60 * 30); // One bonus point every 30 minutes
-var bonusPointsEl = $('.fa-gift').parents('a');
-var bonusPoints = bonusPointsEl.text().replace(/\s/, '');
-var secondsUntilOneThousandBonusPoints = (1000 - bonusPoints) * secondsPerBonusPoint;
+    var secondsPerBonusPoint = (60 * 30); // One bonus point every 30 minutes
+    var bonusPointsEl = $('.fa-gift').parents('a');
+    var bonusPoints = bonusPointsEl.text().replace(/\s/, '');
+    var secondsUntilOneThousandBonusPoints = (1000 - bonusPoints) * secondsPerBonusPoint;
 
-// Calculate when we can redeem the bonus points
-var date = new Date();
-date.setTime(date.getTime() + secondsUntilOneThousandBonusPoints * 1000)
+    // Calculate when we can redeem the bonus points
+    var date = new Date();
+    date.setTime(date.getTime() + secondsUntilOneThousandBonusPoints * 1000);
 
-bonusPointsEl.after(' (Redeem on ' + date.toDateString()  + ')');
+    bonusPointsEl.after(' (Redeem on ' + date.toDateString()  + ')');
+})();
