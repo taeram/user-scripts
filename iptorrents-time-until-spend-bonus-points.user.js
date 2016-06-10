b60e45c79e348653696b4bc01eec2677360fdfdd
Jesse Patching <taeram@users.noreply.github.com>
2016-06-10 15:16:59 -0600

    More waiting until page load

--- iptorrents-time-until-spend-bonus-points.user.js
+++ iptorrents-time-until-spend-bonus-points.user.js
@@ -4,9 +4,9 @@
 // @description     Calculate the number of days until you should redeem your bonus points for 10GB credit
 // @match           https://www.iptorrents.com/*
 // @match           https://iptorrents.com/*
-// @grant           none
+// @grant           unsafeWindow
 // @copyright       Jesse Patching
-// @version         1.2.1
+// @version         1.2.2
 // @license         MIT https://github.com/taeram/user-scripts/blob/master/LICENSE
 // @updateURL       https://raw.github.com/taeram/user-scripts/master/iptorrents-time-until-spend-bonus-points.user.js
 // @downloadURL     https://raw.github.com/taeram/user-scripts/master/iptorrents-time-until-spend-bonus-points.user.js
@@ -15,14 +15,24 @@
 (function() {
     'use strict';
 
-    var secondsPerBonusPoint = (60 * 30); // One bonus point every 30 minutes
-    var bonusPointsEl = $('.fa-gift').parents('a');
-    var bonusPoints = bonusPointsEl.text().replace(/\s/, '');
-    var secondsUntilOneThousandBonusPoints = (1000 - bonusPoints) * secondsPerBonusPoint;
+    // Wait for jQuery to finish loading
+    var jQueryInterval = setInterval(function () {
+        if (typeof unsafeWindow['$'] !== 'undefined') {
+            clearInterval(jQueryInterval);
+            main();
+        }
+    }, 100);
 
-    // Calculate when we can redeem the bonus points
-    var date = new Date();
-    date.setTime(date.getTime() + secondsUntilOneThousandBonusPoints * 1000);
+    function main () {
+        var secondsPerBonusPoint = (60 * 30); // One bonus point every 30 minutes
+        var bonusPointsEl = $('.fa-gift').parents('a');
+        var bonusPoints = bonusPointsEl.text().replace(/\s/, '');
+        var secondsUntilOneThousandBonusPoints = (1000 - bonusPoints) * secondsPerBonusPoint;
 
-    bonusPointsEl.after(' (Redeem on ' + date.toDateString()  + ')');
+        // Calculate when we can redeem the bonus points
+        var date = new Date();
+        date.setTime(date.getTime() + secondsUntilOneThousandBonusPoints * 1000);
+
+        bonusPointsEl.after(' (Redeem on ' + date.toDateString()  + ')');
+    }
 })();
