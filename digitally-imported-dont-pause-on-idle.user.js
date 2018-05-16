2a63938f4d0551d53a60ecf2643dc191b0b49394
Jesse Patching <taeram@users.noreply.github.com>
2018-05-16 13:25:36 -0600

    Update digitally-imported-dont-pause-on-idle.user.js

--- digitally-imported-dont-pause-on-idle.user.js
+++ digitally-imported-dont-pause-on-idle.user.js
@@ -3,9 +3,10 @@
 // @namespace       http://github.com/taeram/user-scripts
 // @description     Don't pause playback on idle
 // @match           http://www.di.fm/*
+// @match           https://www.di.fm/*
 // @grant           none
 // @copyright       Jesse Patching
-// @version         1.0.2
+// @version         1.0.3
 // @license         MIT https://github.com/taeram/user-scripts/blob/master/LICENSE
 // @updateURL       https://raw.github.com/taeram/user-scripts/master/digitally-imported-dont-pause-on-idle.user.js
 // @downloadURL     https://raw.github.com/taeram/user-scripts/master/digitally-imported-dont-pause-on-idle.user.js
