35df17bb99afb3208f1a314b42a9fdcc50c7af7b
Jesse Patching <taeram@users.noreply.github.com>
2018-06-04 15:55:34 -0600

    Update environment-canada-reload-every-hour.user.js

--- environment-canada-reload-every-hour.user.js
+++ environment-canada-reload-every-hour.user.js
@@ -2,32 +2,32 @@
 // @name            Environment Canada - Reload every hour
 // @namespace       http://github.com/taeram/user-scripts
 // @description     Reloads the Environment Canada website once an hour
-// @match           http://weather.gc.ca/city/pages/*
+// @match           https://weather.gc.ca/city/pages/*
 // @grant           none
 // @copyright       Jesse Patching
-// @version         1.0.2
+// @version         1.0.3
 // @license         MIT https://github.com/taeram/user-scripts/blob/master/LICENSE
 // @updateURL       https://raw.github.com/taeram/user-scripts/master/environment-canada-reload-every-hour.user.js
 // @downloadURL     https://raw.github.com/taeram/user-scripts/master/environment-canada-reload-every-hour.user.js
-// ==/UserScript==  
+// ==/UserScript==
 
 /* jshint -W097 */
 'use strict';
 
 var reloadAfter = 3600; // seconds
 var secondsElapsed = 0;
-var timerElement = $('<div style="position: absolute; right: 5px; top: 5px; color: black; font-size: 9px" id="reloadTimer">' + 
-                       'Reloading in <span class="time">' + reloadAfter + '</span> seconds' + 
+var timerElement = $('<div style="position: absolute; right: 5px; top: 5px; color: black; font-size: 9px" id="reloadTimer">' +
+                       'Reloading in <span class="time">' + reloadAfter + '</span> seconds' +
                      '</div>').appendTo('body');
 
 var reloadEvent = function () {
     // One more second has passed
     secondsElapsed++;
-    
+
     // Update the reload message
     var secondsUntilReload = (reloadAfter - secondsElapsed);
     $(timerElement).find('.time').text(secondsUntilReload);
-    
+
     // Reload if we've hit the limit
     if (secondsElapsed >= reloadAfter) {
         window.location.reload();
@@ -35,4 +35,3 @@ var reloadEvent = function () {
 };
 
 setInterval(reloadEvent, 1000)
-    
