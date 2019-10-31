8e816f44e7b1468cce99b11e28458b194669226d
Jesse Patching <taeram@users.noreply.github.com>
2019-10-31 10:11:54 -0600

    Create progress-quest.user.js

---
+++ progress-quest.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name Progress Quest Helper
+// @namespace https://github.com/taeram/user-scripts/
+// @version 1.0.0
+// @description Progress Quest Helper
+// @author Jesse Patching
+// @match http://progressquest.com/play/main.html*
+// @grant none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+})();
