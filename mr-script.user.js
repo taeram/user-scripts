5d68836640d0e4215a472c653d4b50e668814c68
Jesse Patching <taeram@users.noreply.github.com>
2020-07-13 11:38:04 -0600

    Bump version to 1.82 (again)

--- mr-script.user.js
+++ mr-script.user.js
@@ -2,7 +2,7 @@
 // @name         Mr. Script
 // @namespace    http://www.noblesse-oblige.org/lukifer/scripts/
 // @description  Kingdom of Loathing - User Interface Overhaul
-// @version      1.81
+// @version      1.82
 // @license      GPL (http://www.gnu.org/copyleft/gpl.html)
 // @author       Lukifer
 // @contributor  Ohayou
